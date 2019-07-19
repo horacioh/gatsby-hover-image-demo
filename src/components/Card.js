787b3b3ec82bfb573dd23824bc4c78a505a2debb
Horacio Herrera <me@hherrerag.com>
2019-07-19 14:19:21 +0200

    using all the transform declarations still not working :(

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,29 +22,13 @@ function Card({ file }) {
               opacity: 1,
             },
             img: {
-              transform: `scale(1.2)`,
+              transform: `scale(2)`,
             },
           },
         }}
       >
-        <div
-          sx={{
-            backgroundColor: `rgba(0,0,0,0.5)`,
-            position: "absolute",
-            top: 2,
-            right: 2,
-            bottom: 2,
-            left: 2,
-            zIndex: 2,
-            transition: `opacity ease 0.25s`,
-            opacity: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        />
         <Img
-          sx={{ img: { transition: `all .2s ease-in-out`, transitionDelay: 0 } }}
+          sx={{ 'img': { transitionDelay: 0, transitionDuration: '1s', transitionProperty: 'transform', transitionTimingFunction: 'ease-in-out' }}}
           fluid={file.childImageSharp.fluid}
         />
       </div>
